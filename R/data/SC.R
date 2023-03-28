017538cf9b0edf42cfc7d680903532aa2dab62a0
pslebre <pierre-stephane.lebre@ac-dijon.fr>
2023-03-28 14:12:00 +0200

    maj service civique 2022

--- R/data/SC.R
+++ R/data/SC.R
@@ -4,6 +4,29 @@ library(lubridate)
 library(ggplot2)
 library(tidyverse)
 #library(plotly)
+#devtools::install_github("joelgombin/banR", build_vignettes = TRUE)
+# Traitement QPV
+#base2023 <- read_ods("C:/Users/plebre/Documents/projets R/service civique/2023/liste_contrats_valides_2023_03_27_11_30_40.ods")
+# ou load("sc2023.RData")
+
+# sc2023 <- base2023 %>% select(CTV_NUMERO,COO_NUMERO_VOIE_VOL,LIBELLE_VOL,COO_LIBELLE_VOIE_VOL,CODEPOSTALCEDEX_VOL,COMMUNE_VOL) %>%
+#   mutate(adresse=toupper( 
+#     ifelse(is.na(COO_NUMERO_VOIE_VOL) | COO_NUMERO_VOIE_VOL==0, 
+#            str_trim(paste(LIBELLE_VOL,COO_LIBELLE_VOIE_VOL) ),
+#            str_trim( paste(COO_NUMERO_VOIE_VOL,LIBELLE_VOL,COO_LIBELLE_VOIE_VOL)) ) ) ,
+#     adresse=enc2native(adresse) )
+#attention de laisser dans l'ordre et supprimer première colonne
+#sc2023 <- sc2023 %>% select ("v1"=1,"v2"=5,"v3"=6,"v4"=7,"v5"=2) 
+
+### PENSER à SUPPRIMER 1ERE COLONNE
+# write.csv2(sc2023,"C:/Users/plebre/Documents/projets R/service civique/2023/sc2023.csv")
+
+# appliquer géocodage sur sigville et récupérer le fichier
+#SC_sig <- read.csv2("C:/Users/plebre/Documents/projets R/service civique/2023/sc2023_QP_230327_1849.csv",as.is = T)
+
+#jointure sur la base d'origine
+basebfc <- left_join(base2023,SC_sig,by=c("CTV_NUMERO"="X_IDENTIFIANT") )
+#on sait maintenant quelles adresses sont en QP
 
 depbfc <- c("21","25","39","58","70","71","89","90")
 dipl <- c("Sortie en cours de terminale ou Bac non validé (IV)",
@@ -13,64 +36,89 @@ dipl <- c("Sortie en cours de terminale ou Bac non validé (IV)",
           "Sortie en cours de 1er cycle de l'enseignement secondaire (6ème à 4ème) (VI)",
           "Sortie en 3ème (Vbis)",
           "CAP-BEP validé (V)")
-base2020 <- read.csv2("I:/SUPPORT/04_STATS/engagement/Service Civique/liste_contrats_valides_2021_11_08_10_34_21.geocoded.csv",as.is = T)
 
 
-basebfc <- base2020 %>% filter(CTV_DATE_DEBUT != "" & substr(dmy((CTV_DATE_DEBUT)),1,4) < "2021") %>%
-  select(AGR_NUMERO,CTV_NUMERO,CTV_ETAT,CIV_LIBCOURT,CTV_DATE_DEBUT,CTV_DUREE_HEBDO,CTV_BOURSIER,
-         COO_NUMERO_VOIE_VOL,LIBELLE_VOL,COO_LIBELLE_VOIE_VOL,COO_CPLT_ADRESSE_VOL,CODEPOSTALCEDEX_VOL,COMMUNE_VOL,DEPT_VOL,
-         VOL_DATE_NAISSANCE,VOL_PAYS_NATIONALITE,VOL_PAYS_NAISSANCE,NIF_LIBELLE,SIF_LIBELLE,SAC_LIBELLE,MRC_MOTIF_RUPTURE,
-         mission=NO_MISSION1,CODE_POST_MISSION1,PAYS_MISSION1, 
-         ENA_LIBELLE_COURT,CTV_SERVICE_CIVIQUE_INITIATIVE,Devoirs.Faits,MONALISA,évènement.sportif.majeur,Réfugiés.Volont.R,
-         result_citycode,result_city,latitude,longitude) %>%
+
+#baseSC <- read.csv2("I:/SUPPORT/04_STATS/engagement/Service Civique/liste_contrats_valides_2021_11_08_10_34_21.geocoded.csv",as.is = T)
+
+#on va maintenant géolocaliser les adresses et les lieux de missions 1 (uniquement)
+library(banR)
+
+baseSC <- basebfc %>% 
+  filter(CTV_DATE_DEBUT != "" & substr(dmy((CTV_DATE_DEBUT)),1,4) < "2023") %>% #contrats commencés jusqu'en 2022
+  select(AGR_NUMERO,CTV_NUMERO,CTV_ETAT,CIV_LIBCOURT,CTV_DATE_DEBUT,CTV_DUREE_HEBDO,CTV_BOURSIER, #contrat
+         COO_NUMERO_VOIE_VOL,LIBELLE_VOL,COO_LIBELLE_VOIE_VOL,COO_CPLT_ADRESSE_VOL,CODEPOSTALCEDEX_VOL,COMMUNE_VOL,DEPT_VOL, 
+         VOL_DATE_NAISSANCE,VOL_PAYS_NATIONALITE,VOL_PAYS_NAISSANCE,NIF_LIBELLE,SIF_LIBELLE,SAC_LIBELLE, #volontaire
+         MRC_MOTIF_RUPTURE,mission=NO_MISSION1,CODE_POST_MISSION1,PAYS_MISSION1, #mission
+         ENA_LIBELLE_COURT,CTV_SERVICE_CIVIQUE_INITIATIVE,
+         `Devoirs Faits`:`Etablissements d'enseignement supérieur - Inclusion des étudiants en situation de handicap`,
+         NOM_VOIE, CODE_COMMUNE_INSEE,NOM_COMMUNE_REF ,# pour géocodage
+         LOCCOM_REF:TYPE_QUARTIER ) %>%  #QPV
   mutate_if(is.integer,as.character) %>% 
   mutate(CTV_DUREE_HEBDO=as.numeric(CTV_DUREE_HEBDO),
-         depvol=substr( ifelse(nchar(result_citycode)==4,paste0("0",result_citycode),result_citycode) ,1 ,2 ) ,
-         depmis=ifelse(PAYS_MISSION1 !="FRANCE","ETRANGER", substr (
-                               ifelse(nchar(CODE_POST_MISSION1)==4 ,paste0("0",CODE_POST_MISSION1),CODE_POST_MISSION1)  ,1 ,2 ) ) ,
+         depvol= if_else(!is.na(CODE_COMMUNE_INSEE),
+                         substr( ifelse(nchar(CODE_COMMUNE_INSEE)==4,
+                                        paste0("0",CODE_COMMUNE_INSEE),
+                                        CODE_COMMUNE_INSEE) ,
+                                 1 ,2 ),
+                         ifelse(nchar(DEPT_VOL)==1, 
+                                paste0("0",DEPT_VOL), 
+                                DEPT_VOL) ) ,
+         depmis=ifelse(PAYS_MISSION1 !="FRANCE","ETRANGER", 
+                       substr (ifelse(nchar(CODE_POST_MISSION1)==4 ,
+                                      paste0("0",CODE_POST_MISSION1),
+                                      CODE_POST_MISSION1)  ,
+                               1 ,2 ) ) ,
          agre=substr(AGR_NUMERO,1,2),
          debut= substr(CTV_DATE_DEBUT,7,11),
          mission =  ifelse(mission=="03 - Education pour tous","1-Education pour tous",
                            ifelse(mission=="01 - Solidarité","2-Solidarité",
-                           ifelse(mission=="05 - Sport","3-Sport",
-                           ifelse(mission=="04 - Culture et loisirs","4-Culture & loisirs",
-                           ifelse(mission=="07 - Mémoire et citoyenneté","5-Mémoire & citoyenneté",
-                           ifelse(mission=="06 - Environnement","6-Environnement",
-                           ifelse(mission=="02 - Santé","7-Santé",
-                           ifelse(mission=="09 - Intervention d'urgence","8-Intervention d'urgence",
-                           ifelse(mission=="08 - Développement international et action humanitaire",
-                                          "9-Dvt international & action humanitaire",substr(mission,2,100) ))))))))) ,
+                            ifelse(mission=="05 - Sport","3-Sport",
+                            ifelse(mission=="04 - Culture et loisirs","4-Culture & loisirs",
+                            ifelse(mission=="07 - Mémoire et citoyenneté","5-Mémoire & citoyenneté",
+                            ifelse(mission=="06 - Environnement","6-Environnement",
+                            ifelse(mission=="02 - Santé","7-Santé",
+                            ifelse(mission=="09 - Intervention d'urgence","8-Intervention d'urgence",
+                            ifelse(mission=="08 - Développement international et action humanitaire",
+                            "9-Dvt international & action humanitaire",
+                            substr(mission,1,100) ))))))))) ,
          age=time_length(dmy(VOL_DATE_NAISSANCE) %--% dmy(CTV_DATE_DEBUT),"years")
-         )
-basebfc$depvol <- ifelse(is.na(basebfc$depvol),basebfc$DEPT_VOL,basebfc$depvol)
+  )
 
-tableau <- basebfc %>%filter(depmis %in% depbfc ) %>%
+save(baseSC,oursin,oursinxy,flows,file="data/engagement/SC.RData")
+
+
+
+tableau <- baseSC %>% 
+  filter(depmis %in% depbfc ) %>%
   group_by(depmis) %>%
-  summarise(tot=n(),tot20=sum(debut==2020),
-            mage=mean(age),mage20=mean(age[debut==2020]),
-            femmes=100*sum(CIV_LIBCOURT =="MME")/tot,femmes20=100*sum(CIV_LIBCOURT[debut==2020] =="MME")/tot20,
-            peudipl=100*sum(NIF_LIBELLE  %in% dipl )/tot,peudipl20=100*sum(NIF_LIBELLE[debut==2020]  %in% dipl )/tot20,
-            sitfam=100*sum(SIF_LIBELLE =="Célibataire" )/tot,sitfam20=100*sum(SIF_LIBELLE[debut==2020] =="Célibataire" )/tot20,
-            chomage=100*sum(SAC_LIBELLE =="Demandeur d'emploi" )/tot,chomage20=100*sum(SAC_LIBELLE[debut==2020] =="Demandeur d'emploi"  )/tot20,
+  summarise(tot=n(),tot22=sum(debut==2022),
+            mage=mean(age),mage22=mean(age[debut==2022]),
+            femmes=100*sum(CIV_LIBCOURT =="MME")/tot,femmes22=100*sum(CIV_LIBCOURT[debut==2022] =="MME")/tot22,
+            peudipl=100*sum(NIF_LIBELLE  %in% dipl )/tot,peudipl22=100*sum(NIF_LIBELLE[debut==2022]  %in% dipl )/tot22,
+            sitfam=100*sum(SIF_LIBELLE =="Célibataire" )/tot,sitfam22=100*sum(SIF_LIBELLE[debut==2022] =="Célibataire" )/tot22,
+            chomage=100*sum(SAC_LIBELLE =="Demandeur d'emploi" )/tot,chomage22=100*sum(SAC_LIBELLE[debut==2022] =="Demandeur d'emploi"  )/tot22,
             rupture=sum(MRC_MOTIF_RUPTURE != " - "),txrupture=100*rupture/tot,
             embauche=100*sum(MRC_MOTIF_RUPTURE %in% c(
               "04 - Embauche en CDD d'au moins 6 mois ou CDI ",
               "05 - Embauche en CDD moins de 6 mois")/rupture )
-  ) %>% bind_rows( basebfc %>%filter(depmis %in% depbfc ) %>% group_by() %>%
-  summarise(depmis="BFC",tot=n(),tot20=sum(debut==2020),
-            mage=mean(age),mage20=mean(age[debut==2020]),
-            femmes=100*sum(CIV_LIBCOURT =="MME")/tot,femmes20=100*sum(CIV_LIBCOURT[debut==2020] =="MME")/tot20,
-            peudipl=100*sum(NIF_LIBELLE  %in% dipl )/tot,peudipl20=100*sum(NIF_LIBELLE[debut==2020]  %in% dipl )/tot20,
-            sitfam=100*sum(SIF_LIBELLE =="Célibataire" )/tot,sitfam20=100*sum(SIF_LIBELLE[debut==2020] =="Célibataire" )/tot20,
-            chomage=100*sum(SAC_LIBELLE =="Demandeur d'emploi" )/tot,chomage20=100*sum(SAC_LIBELLE[debut==2020] =="Demandeur d'emploi"  )/tot20,
-            rupture=sum(MRC_MOTIF_RUPTURE != " - "),txrupture=100*rupture/tot,
-            embauche=100*sum(MRC_MOTIF_RUPTURE %in% c(
-              "04 - Embauche en CDD d'au moins 6 mois ou CDI ",
-              "05 - Embauche en CDD moins de 6 mois")/rupture ) ) )
+  ) %>% bind_rows( baseSC %>%filter(depmis %in% depbfc ) %>% group_by() %>%
+                     summarise(depmis="BFC",tot=n(),tot22=sum(debut==2022),
+                               mage=mean(age),mage22=mean(age[debut==2022]),
+                               femmes=100*sum(CIV_LIBCOURT =="MME")/tot,femmes22=100*sum(CIV_LIBCOURT[debut==2022] =="MME")/tot22,
+                               peudipl=100*sum(NIF_LIBELLE  %in% dipl )/tot,peudipl22=100*sum(NIF_LIBELLE[debut==2022]  %in% dipl )/tot22,
+                               sitfam=100*sum(SIF_LIBELLE =="Célibataire" )/tot,sitfam22=100*sum(SIF_LIBELLE[debut==2022] =="Célibataire" )/tot22,
+                               chomage=100*sum(SAC_LIBELLE =="Demandeur d'emploi" )/tot,chomage22=100*sum(SAC_LIBELLE[debut==2022] =="Demandeur d'emploi"  )/tot22,
+                               rupture=sum(MRC_MOTIF_RUPTURE != " - "),txrupture=100*rupture/tot,
+                               embauche=100*sum(MRC_MOTIF_RUPTURE %in% c(
+                                 "04 - Embauche en CDD d'au moins 6 mois ou CDI ",
+                                 "05 - Embauche en CDD moins de 6 mois")/rupture ) ) )
 
 
 
-vol <-   basebfc %>% filter(depvol %in% depbfc ) %>% mutate(dept_vol=as.character(depvol)) %>%
+vol <-   baseSC %>% 
+  filter(depvol %in% depbfc ) %>% 
+  mutate(dept_vol=as.character(depvol)) %>%
   group_by(depvol,debut) %>%
   summarise(volresid=n()) 
 
@@ -82,7 +130,7 @@ p <- ggplot(vol,aes(debut,y=volresid,group=depvol,color=depvol)  ) +
   labs(caption = "Source : Agence du Service Civique, Elisa, traitement DRDJSCS",
        x="",y="volontaires au lieu de résidence",colour="département") +
   # theme_dark()   +
-  scale_x_discrete(breaks=2010:2020) +
+  scale_x_discrete(breaks=2010:2022) +
   theme(
     #panel.background = element_rect(fill = "grey15"),
     legend.key = element_rect(fill = "grey45"),
@@ -95,7 +143,8 @@ p
 
 ggplotly(p,  dynamicTicks = T )
 
-int_miss <-   basebfc %>% filter(depvol %in% depbfc & depmis %in% depbfc)    %>%
+int_miss <-   baseSC %>% 
+  filter(depvol %in% depbfc & depmis %in% depbfc)    %>%
   filter() %>%
   group_by(mission,debut) %>%
   count() 
@@ -109,7 +158,7 @@ p <- ggplot(int_miss,aes(x=debut,y=n,fill=mission,color=mission ) ) +
        x="année",y="volontaires au lieu de résidence",colour="département") +
   #  theme_dark()   +
   #    scale_color_jcolors(palette = "pal8")+
-  scale_x_discrete(breaks=2010:2020)+
+  scale_x_discrete(breaks=2010:2022)+
   coord_polar("y",start=0) +
   theme(legend.position = "bottom",
         legend.key.size=unit(0.5, "cm"),
@@ -121,8 +170,8 @@ p
 #ggplotly(p)
 
 
-duree <-   basebfc %>% filter(depvol %in% depbfc) %>%
-  filter() %>%
+duree <-   baseSC %>% 
+  filter(depvol %in% depbfc) %>%
   group_by(debut,depvol) %>%
   summarise(mduree=mean(CTV_DUREE_HEBDO,na.rm=T)) 
 
@@ -146,53 +195,14 @@ p <- ggplot(duree,aes(x=debut,y=mduree,group=depvol,color=depvol))+ ylim(23,31)+
 #scale_color_jcolors(palette = "pal6")
 p
 
-ggplotly(p)
-
-duree2 <- spread(duree,debut,mduree)
 
 
-
-table(basebfc1$CIV_LIBCOURT)
-a <-as.data.frame( table(basebfc1$NIF_LIBELLE) )
-
-table(basebfc1$SIF_LIBELLE)
-table(basebfc1$SAC_LIBELLE)
-table(basebfc1$MRC_MOTIF_RUPTURE)
-
-rpivotTable(basebfc1)
-
-
-
-
-
-### Traitement QPV
-
-#baseqpv <- base2020 %>% select(CTV_NUMERO,COO_NUMERO_VOIE_VOL,LIBELLE_VOL,COO_LIBELLE_VOIE_VOL,CODEPOSTALCEDEX_VOL,COMMUNE_VOL) %>%
-#  mutate(adresse=toupper( 
-#    ifelse(is.na(COO_NUMERO_VOIE_VOL) | COO_NUMERO_VOIE_VOL==0, 
-#           str_trim(paste(LIBELLE_VOL,COO_LIBELLE_VOIE_VOL) ),
-#           str_trim( paste(COO_NUMERO_VOIE_VOL,LIBELLE_VOL,COO_LIBELLE_VOIE_VOL)) ) ) ,
-#    adresse=enc2native(adresse) )
-#attention de laisser dans l'ordre et supprimer première colonne
-# baseqpv <- baseqpv %>% select ("v1"=1,"v2"=5,"v3"=6,"v4"=7,"v5"=2) 
-
-### PENSER à SUPPRIMER 1ERE COLONNE
-#write.csv2(baseqpv,"I:/SUPPORT/04_STATS/engagement/Service Civique/baseqpv.csv")   
-
-
-basesig <- read.csv2("I:/SUPPORT/04_STATS/engagement/Service Civique/baseqpv_QP_211108_1419.csv",as.is = T)
-
-basebfcsig <- left_join(basebfc,basesig,by=c("CTV_NUMERO"="X_IDENTIFIANT") )
-
-#write.dbf(basebfc,"/home/ps/Documents/teletravail/réseau/JEUNESSE/SERVICE CIVIQUE/base2020/baseqpvverif.dbf")
-
-addmargins(table(basesig$TYPE_QUARTIER,basesig$LOCADR_REF) )
-table(basesig$LOCADR_REF)
 
 qp <- c("VOIE_EN_LIMITE","VOIE_EN_PARTIE","VOIE_ENTIERE") 
 qp <- c("VOIE_ENTIERE") 
 
-tableauqpv <- basebfc %>% mutate(dep_v=substr(CODE_COMMUNE_INSEE,1,2) ) %>%
+tableauqpv <- baseSC %>% 
+  mutate(dep_v=substr(CODE_COMMUNE_INSEE,1,2) ) %>%
   filter(dep_v %in% depbfc ) %>% 
   group_by(dep_v) %>%
   summarise(qpv=sum(!(CODE_QUARTIER=="")), n() ,tx= 100*qpv/n() 
@@ -202,7 +212,7 @@ write.csv(tableauqpv,"C:/Users/plebre/Documents/projets R/service civique/2020/t
 rpivotTable::rpivotTable(basebfc%>% mutate(dep_v=substr(CODE_COMMUNE_INSEE,1,2) ))  
 
 
-save(basebfc,basebfcsig,oursin,oursinxy,flows,file="data/engagement/SC.RData")
+save(baseSC,basebfc,basebfcsig,oursin,oursinxy,flows,file="data/engagement/SC.RData")
 
 library(geosphere)
 library(rgdal)
