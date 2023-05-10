66d5b1c4b7f029d1418212ee90d14758930c4ac3
pslebre <pierre-stephane.lebre@ac-dijon.fr>
2023-05-10 10:13:39 +0200

    bug config rgeos

--- dev/render_site.R
+++ dev/render_site.R
@@ -1,10 +1,8 @@
 # renv::snapshot()     #renv sert à installer les packages lors du déployment github actions
 
-if (!requireNamespace("renv", quietly = TRUE)) install.packages("renv") 
+#if (!requireNamespace("renv", quietly = TRUE)) install.packages("renv") 
 
 renv::restore() #on restaure les packages qu'on va utiliser pour construire (voir gh-pages.yml)
-library(ForestTools)
-
 
 rmarkdown::clean_site(encoding = 'UTF-8')
 rmarkdown::render_site(encoding = 'UTF-8')
