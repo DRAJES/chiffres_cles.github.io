40db48841f8191aa0c6366bbf22c7a630bde69d5
pslebre <pierre-stephane.lebre@ac-dijon.fr>
2023-05-30 16:58:39 +0200

    ajout du barometre de l'action publique

---
+++ docs/site_libs/transceiver-binding-0.0.0/transceiver.js
@@ -0,0 +1,61 @@
+HTMLWidgets.widget({
+  name: 'transceiver',       // see also transmitter, receiver
+  type: 'output',
+
+  factory: function(el, width, height) {
+    var antenna = {channel_1: null, channel_2: null};
+    return {
+      renderValue: function(x) {
+        if(x.crosstalk_group) { 
+          if(x.channel == "filter") {
+            antenna.channel_1 = new crosstalk.FilterHandle();
+            antenna.channel_2 = new crosstalk.FilterHandle();
+          } else if(x.channel == "filter->select") {
+            antenna.channel_1 = new crosstalk.FilterHandle();
+            antenna.channel_2 = new crosstalk.SelectionHandle();
+          } else if(x.channel == "select->filter") {
+            antenna.channel_1 = new crosstalk.SelectionHandle();
+            antenna.channel_2 = new crosstalk.FilterHandle();
+          } else {
+            antenna.channel_1 = new crosstalk.SelectionHandle();
+            antenna.channel_2 = new crosstalk.SelectionHandle();
+          }
+          antenna.channel_1.setGroup(x.crosstalk_group);
+          antenna.channel_2.setGroup(x.crosstalk_group2);
+        } else return;
+        el.innerHTML = x.innerHTML;
+        var cf = function(e) {
+          if(e.sender === antenna.channel_1 || e.sender === antenna.channel_2) return;
+          if(x.alldone) return;
+          if(x.reset && e.sender) {
+            antenna.channel_2.set(x.reset);
+            x.alldone = true;
+            return;
+          }
+          var val;
+          if(Array.isArray(e.value)) {
+            // the usual crosstalk selection array values
+            if(x.lookup) {
+              val = e.value.map(function(i) {return x.lookup[x.crosstalk_key.indexOf(i)];});
+            } else val = e.value;
+            if(! Array.isArray(val)) val = [val];
+              if(Array.isArray(val[0])) val = [].concat.apply([], val); // flatten
+          } else val = e.value;
+          if(el.children && el.children.length > 0) el.children[0][x.value] = val;
+         antenna.channel_2.set(val);
+        };
+        antenna.channel_1.on("change", cf);
+        if(el.children && el.children.length > 0) {
+          el.children[0].addEventListener("change", function() {
+            var val = el.children[0][x.value];
+            if(!Array.isArray(val)) val = [val];
+            if(Array.isArray(val[0])) val = [].concat.apply([], val); // flatten
+            antenna.channel_2.set(val); // send HTML element values
+          });
+        }
+        if(x.init) cf({sender: null, value: [x.init]});
+      },
+      resize: function(width, height) { }
+    };
+  }
+});
