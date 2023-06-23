a902d45dfa156321804208585665e879d24fa842
pslebre <pierre-stephane.lebre@ac-dijon.fr>
2023-06-23 11:52:40 +0200

    ajout asso employeuses

--- R/fonctions/fonctions_carto.R
+++ R/fonctions/fonctions_carto.R
@@ -1,6 +1,7 @@
 contour_bfc <-  function(.map){ 
   .map %>% 
-    addProviderTiles(providers$CartoDB.Positron) %>%
+    addProviderTiles(providers$CartoDB.Positron,
+                     options = tileOptions(minZoom=6, maxZoom=9)) %>%
     setView(lng = 5.1, lat = 47.27, zoom = 6) %>%
     addMapPane("departement", zIndex = 430) %>%
     addPolygons(data=reg27carto,group  = "BFC",
@@ -12,6 +13,8 @@ contour_depbfc <-   function(.map,zoom = 8){
   .map %>% 
     contour_bfc() %>%
     setView(lng = 5.1, lat = 47.27, zoom = zoom) %>%
+    addProviderTiles(providers$CartoDB.Positron,
+                     options = tileOptions(minZoom=7, maxZoom=14)) %>%
     addMapPane("region", zIndex = 420) %>%
     addPolygons(data=dep27carto,group  = "BFC",
                 color="#800080",weight = 3, opacity = 1, 
